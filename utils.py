aca048a155e2fa5bccf67647bebcafc514f795c2
Bogdan <hakker012@gmail.com>
2026-04-23 17:12:20 +0300

    added utils for validation functions

---
+++ utils.py
@@ -0,0 +1,14 @@
+def _check_img_file_extension():
+    pass
+
+def _img_hash():
+    pass
+
+def _check_size_limits():
+    pass
+
+def _check_duplicates(img_folder: str):
+    pass
+
+def save_img(img_folder: str):
+    pass
